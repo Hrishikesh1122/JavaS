8eec38b13c6f29c3c7722cf8624ab0c683538cc7
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-05-31 08:58:41 +0530

    Push1

---
+++ JavaPractice1/src/main/java/com/blz/javapractice1/App.java
@@ -0,0 +1,23 @@
+package com.blz.javapractice1;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{ 
+	private static final Logger LOG = LogManager.getLogger(App.class);
+    public static void main( String[] args )
+    {
+    	String message = "Hello World ";
+        LOG.debug(message + "Will show debug message");
+		LOG.info(message + "Will show info message");
+		LOG.warn(message+"Will show warn message");
+		LOG.error(message+"Will show error message");
+		LOG.fatal(message+"Will show fatal message");
+		LOG.info("Appending String {}", message);
+		System.out.println(message);
+    }
+}
---
+++ hello java/src/HelloJava.java
@@ -0,0 +1,13 @@
+
+public class HelloJava { 
+	public static void main(String[] args) {
+	
+	System.out.println("Hello Java");
+	System.out.println("Hello java!!");
+	}
+	
+	public int add (int a,int b)
+	{
+		return a+b;
+	}
+}
